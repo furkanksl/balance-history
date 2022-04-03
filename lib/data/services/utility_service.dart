fa45728d1140dc5e750fb6709b3ad7db9bb2d188
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-04-03 15:16:12 +0300

    Upgrade utility service

--- lib/data/services/utility_service.dart
+++ lib/data/services/utility_service.dart
@@ -1,18 +1,28 @@
 import 'package:balance_history/domain/entities/history.dart';
 import 'package:intl/intl.dart';
 
+enum ORDER_TYPE {
+  daily,
+  monthly,
+  yearly,
+}
+
 class UtilityService {
-  DateTime toDateTime(String date) => DateFormat(
-        'yyyy-MM-ddTHH:mm:ss',
-      ).parse(date);
+  static const oneBtcToUsd = 50.000;
+
+  DateTime toDateTime(String date) =>
+      DateFormat('yyyy-MM-ddTHH:mm:ss').parse(date);
 
   List<History> toDailyBalanceHistory(List<History> list) {
     List<History> dailyOrderedList = [list[0]];
+
     for (var i = 1; i < list.length; i++) {
       dailyOrderedList.add(list[i]);
+
       bool areDaysSame = list[i].dateTime!.day == list[i - 1].dateTime!.day;
       bool areMonthsSame =
           list[i].dateTime!.month == list[i - 1].dateTime!.month;
+
       if (areDaysSame && areMonthsSame) {
         dailyOrderedList.remove(list[i - 1]);
       }
@@ -23,11 +33,14 @@ class UtilityService {
 
   List<History> toMonthlyBalanceHistory(List<History> list) {
     List<History> monthlyOrderedList = [list[0]];
+
     for (var i = 1; i < list.length; i++) {
       monthlyOrderedList.add(list[i]);
+
       bool areYearsSame = list[i].dateTime!.year == list[i - 1].dateTime!.year;
       bool areMonthsSame =
           list[i].dateTime!.month == list[i - 1].dateTime!.month;
+
       if (areYearsSame && areMonthsSame) {
         monthlyOrderedList.remove(list[i - 1]);
       }
@@ -38,23 +51,44 @@ class UtilityService {
 
   List<History> toYearlyBalanceHistory(List<History> list) {
     List<History> yearlyOrderedList = [list[0]];
+
     for (var i = 1; i < list.length; i++) {
       yearlyOrderedList.add(list[i]);
+
       bool areYearsSame = list[i].dateTime!.year == list[i - 1].dateTime!.year;
+
       if (areYearsSame) {
         yearlyOrderedList.remove(list[i - 1]);
       }
     }
+
     return yearlyOrderedList;
   }
 
   num satoshisToBTC(num satoshis) => satoshis * 0.00000001;
+
+  num btcToUSD(num btc) => btc * oneBtcToUsd;
+
+  List<History> getOrderedBalanceHistory({
+    required ORDER_TYPE type,
+    required List<History> list,
+  }) {
+    switch (type) {
+      case ORDER_TYPE.daily:
+        return toDailyBalanceHistory(list).reversed.toList();
+
+      case ORDER_TYPE.monthly:
+        return toMonthlyBalanceHistory(
+          UtilityService().toDailyBalanceHistory(list).reversed.toList(),
+        );
+
+      case ORDER_TYPE.yearly:
+        return toYearlyBalanceHistory(
+          UtilityService().toDailyBalanceHistory(list).reversed.toList(),
+        );
+
+      default:
+        return [];
+    }
+  }
 }
-
-
-  // var mylist = await ApiServiceImpl().getAllBalanceHistory();
-  // print(UtilityService().toDailyBalanceHistory(mylist).reversed.toList());
-  // print(UtilityService().toMonthlyBalanceHistory(
-  //     UtilityService().toDailyBalanceHistory(mylist).reversed.toList()));
-  // print(UtilityService().toYearlyBalanceHistory(
-  //     UtilityService().toDailyBalanceHistory(mylist).reversed.toList()));
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:balance_history/presentation/bloc/history_bloc/history_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'injection.dart' as injector;
 
 void main() async {
   injector.init();
   runApp(const MyApp());
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final int x;
+  final double y;
 }
 
 class MyApp extends StatelessWidget {
@@ -14,14 +21,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData(2010, 35),
+      ChartData(2011, 28),
+      ChartData(2012, 34),
+      ChartData(2013, 32),
+      ChartData(2014, 40)
+    ];
     return MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => injector.locator<HistoryBloc>())],
+      providers: [
+        BlocProvider(
+          create: (_) => injector.locator<HistoryBloc>(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const Scaffold(),
+        home: Scaffold(
+          body: Center(
+            child: SizedBox(
+              width: double.infinity - 10,
+              height: 400,
+              child: SfCartesianChart(
+                series: <SplineSeries>[
+                  // Renders line chart
+                  SplineSeries<ChartData, dynamic>(
+                    dataSource: chartData,
+                    xValueMapper: (ChartData data, _) => data.x,
+                    yValueMapper: (ChartData data, _) => data.y,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
